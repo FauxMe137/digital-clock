5a21b9fd0c2e3e5b514187db7ad014ba4e682b76
Aika Osakioya Oghenevize <osakioyaaika1@gmail.com>
2026-09-22 01:32:49 +0100

    Created Stopwatch component.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,8 @@
-import Header from './Header.jsx';
-import Footer from './Footer.jsx';
-import Food from './Food.jsx';
+import Stopwatch from "./Stopwatch";
 
 function App() {
   return(
-    <>
-      <Header/>
-      <Food/>
-      <Footer/>
-    </>
+    <Stopwatch/>
   );
 }
 
--- my-react-app/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return(
-        <footer>
-            <p>My Website© {new Date().getFullYear()}</p>
-        </footer>
-    );
-}
-
-export default Footer
